39b2a8f4c872809c28a52203be6fe0c946a7e67d
James Aley <james.aley@gmail.com>
2011-06-20 18:39:08 +0100

    Added an empty scala borachio test.

---
+++ test/src/com/touchtype_fluency/examples/borachio_warehouse/test/TestBorachioWarehouse.scala
@@ -0,0 +1,16 @@
+package com.touchtype_fluency.examples.borachio_warehouse.test;
+ 
+import junit.framework.TestCase
+import junit.framework.Assert._
+import android.test.suitebuilder.annotation._
+
+import com.borachio.junit3.MockFactory
+ 
+class TestBorachioWarehouse extends TestCase with MockFactory {
+  
+  @MediumTest
+  def testExample {
+    assertTrue(false)
+  }
+ 
+}
